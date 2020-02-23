58956f45b138fdba326ec497d0af9e12672d1544
suganthan <suganthan.friartuck.net>
2020-02-23 20:13:14 +0800

    Add binary number

---
+++ src/math/binary/AddBinary.java
@@ -0,0 +1,21 @@
+package math.binary;
+
+public class AddBinary {
+    public static void main(String[] args) {
+        System.out.println(addBinary("1010", "1011"));
+    }
+
+    public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() -1, j = b.length() - 1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (j >= 0) sum += b.charAt(j--) - '0'; //we subtract '0' to get the int value of the char from the ascii
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
