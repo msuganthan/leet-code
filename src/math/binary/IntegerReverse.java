b7115a6cd53fa6af6c07b7d709d9be6b2f0d4d4f
suganthan <suganthan.friartuck.net>
2020-02-23 21:35:02 +0800

    Reverse integer

---
+++ src/math/binary/IntegerReverse.java
@@ -0,0 +1,19 @@
+package math.binary;
+
+public class IntegerReverse {
+    public static void main(String[] args) {
+        System.out.println(rever(123));
+    }
+
+    public static int rever(int x){
+        long r = 0;
+        while (x != 0) {
+            r =  r * 10 + x % 10;
+            x /= 10;
+        }
+        if(r >= Integer.MIN_VALUE && r <= Integer.MAX_VALUE)
+			return (int)r;
+		else
+			return 0;
+    }
+}
