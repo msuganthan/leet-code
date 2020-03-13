c4ff4c242785b094c630ced8e6a05713fdc5d9ab
msuganthan <suganthanchandrika@gmail.com>
2020-03-13 13:25:20 +0530

    New problems

---
+++ src/math/removeElements/RemoveElements.java
@@ -0,0 +1,19 @@
+package math.removeElements;
+
+/**
+ * Created by msuganthan on 10/3/20.
+ */
+public class RemoveElements {
+    public static void main(String[] args) {
+        removeElements(new int[]{ 0, 1, 2, 2, 3, 0, 4, 2 }, 2);
+    }
+
+    static int removeElements(int[] A, int elem) {
+        int l = A.length;
+        for (int i = 0; i < l; i++) {
+            if (A[i] == elem)
+                A[i--] = A[l-- -1];
+        }
+        return l;
+    }
+}
---
+++ src/string/StrStr.java
@@ -0,0 +1,21 @@
+package string;
+
+/**
+ * Created by msuganthan on 10/3/20.
+ */
+public class StrStr {
+    public static void main(String[] args) {
+        strStr("hello", "ll");
+    }
+
+    static int strStr(String hayStack, String needle) {
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) return i;
+                if (i + j == hayStack.length()) return  -1;
+                if (needle.charAt(j) != hayStack.charAt( i + j)) break;
+            }
+        }
+
+    }
+}
