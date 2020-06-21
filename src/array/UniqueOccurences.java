bb55783b750fc7d34d6755424a244cfdd9cf57aa
suganthan <suganthan.friartuck.net>
2020-06-22 00:41:04 +0800

    Unique occurrences

--- src/array/UniqueOccurences.java
+++ src/array/UniqueOccurences.java
@@ -1,14 +1,31 @@
 package array;
 
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+/**
+ * Given an array of integers {@code arr}`, write a function that returns {@code true} if and only if the number of
+ * occurrences of each value in the array is unique.
+ */
 public class UniqueOccurences {
     public static void main(String[] args) {
-        Map<int[], Long> collect = Arrays.asList(new int[]{1,2,2,1,1,3}).stream()
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        System.out.println(collect);
+        System.out.println(uniqueOccurrences(new int[] {1,2,2,1,1,3}));
+    }
+
+    static boolean uniqueOccurrences(int[] arr) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> values = new HashSet<>();
+        for (int n: arr)
+            map.put(n, map.getOrDefault(n, 0) + 1);
+
+        for(int val: map.values()) {
+            if (values.contains(val))
+                return false;
+            else
+                values.add(val);
+        }
+        return true;
     }
 }
