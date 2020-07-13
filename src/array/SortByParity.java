657458b45216885944786996e8746593d5d7992b
suganthan <suganthan.friartuck.net>
2020-07-14 07:30:41 +0800

    Parity

--- src/array/SortByParity.java
+++ src/array/SortByParity.java
@@ -2,20 +2,31 @@ package array;
 
 public class SortByParity {
     public static void main(String[] args) {
-        System.out.println(sortByParity(new int[] {1,3, 2,4}));
+        int[] ints = sortByParity(new int[]{1, 3, 2, 4});
+        for (int i: ints)
+            System.out.println(i);
     }
 
     private static int[] sortByParity(int[] A) {
         int i = 0;
-        int j = 0;
-        while(i < A.length) {
-            if (A[i] % 2 == 0) {
-                int tmp = A[i]; //could be an even number
-                A[i]    = A[j]; //placing an odd number in the place of even number.
-                A[j++]  = tmp;
-            }
-            i++;
+        int j = 1;
+        int n = A.length;
+        while(i < n && j < n) {
+
+            while (i < n && A[i] % 2 == 0) i +=2;
+
+            while (j < n && A[j] % 2 != 0) j +=2;
+
+            if (i < n && j < n)
+                swap(i, j, A);
+
         }
         return A;
+    }
+
+    static void swap(int i, int j, int[] A) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
     }
 }
