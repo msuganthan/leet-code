574dc33352892d951ad8f71567ad989d1b8aba47
suganthan <suganthan.friartuck.net>
2020-07-14 17:41:32 +0800

    Unique emails

---
+++ src/string/UniqueEmails.java
@@ -0,0 +1,22 @@
+package string;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueEmails {
+
+    public static void main(String[] args) {
+        System.out.println(numberOfUniqueEmails(new String[]{"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"}));
+    }
+
+    private static int numberOfUniqueEmails(String[] emails) {
+        Set<String> normalized = new HashSet<>();
+        for (String s: emails) {
+            String[] parts = s.split("@");
+            String[] local = parts[0].split("\\+");
+            normalized.add(local[0].replace(".", "") + "@" + parts[1]);
+        }
+
+        return normalized.size();
+    }
+}
