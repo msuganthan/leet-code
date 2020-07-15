ac3f46f881acdc27a2407dd480d478270e866642
suganthan <suganthan.friartuck.net>
2020-07-15 12:27:26 +0800

    Miscellanous

--- src/array/XCardsInDeck.java
+++ src/array/XCardsInDeck.java
@@ -1,4 +1,27 @@
 package array;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class XCardsInDeck {
+
+    public static void main(String[] args) {
+       hasGroupsSizeX(new int[] {1,1,2,2,2,2});
+    }
+
+    public static boolean hasGroupsSizeX(int[] deck) {
+        Map<Integer, Integer> count = new HashMap<>();
+        int res = 0;
+        for (int i : deck) count.put(i, count.getOrDefault(i, 0) + 1);
+        for (int i : count.values()) res = gcd(i, res);
+        return res > 1;
+    }
+
+    public static int gcd(int a, int b) {
+        return b > 0 ? gcd(b, a % b) : a;
+    }
 }
