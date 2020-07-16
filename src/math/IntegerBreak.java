5f4fa307353c860dc3f03607085ec11d39e60ef9
suganthan <suganthan.friartuck.net>
2020-07-16 16:04:40 +0800

    Integer break

---
+++ src/math/IntegerBreak.java
@@ -0,0 +1,20 @@
+package math;
+
+public class IntegerBreak {
+
+    public static void main(String[] args) {
+        integerBreak(8);
+    }
+
+    static int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        int product = 1;
+        while (n >= 4) {
+            product *= 3;
+            n -= 3;
+        }
+        product *= n;
+        return product;
+    }
+}
