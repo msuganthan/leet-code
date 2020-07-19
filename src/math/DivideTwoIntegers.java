ff2bd0430cb4824bc694415db4c7a732ca92c34f
suganthan <suganthan.friartuck.net>
2020-07-19 18:17:06 +0800

    Divide two integers

---
+++ src/math/DivideTwoIntegers.java
@@ -0,0 +1,33 @@
+package math;
+
+public class DivideTwoIntegers {
+
+    public static void main(String[] args) {
+        System.out.println(divide(-2147483648
+-1, -1));
+    }
+
+    public static int divide(int dividend, int divisor) {
+        int sign = (dividend<0)^(divisor<0) ? -1 : 1, quotient = 0;
+        if(dividend == Integer.MIN_VALUE){
+            if(divisor == -1) return Integer.MAX_VALUE;
+            if(divisor == Integer.MIN_VALUE) return 1;
+            dividend += Math.abs(divisor);
+            quotient++;
+        }
+        if(divisor == Integer.MIN_VALUE) return 0;
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor); //Math.abs(-2147483648)=-2147483648
+        while(dividend >= divisor){
+            int tmp = divisor, count = 1;
+            while(dividend - tmp >= tmp){ //(dividend >= tmp<<1 or >= 2*tmp) may cause overflows
+                tmp <<= 1;
+                count <<= 1;
+            }
+            dividend -= tmp;
+            quotient += count;
+        }
+        return sign * quotient;
+
+    }
+}
