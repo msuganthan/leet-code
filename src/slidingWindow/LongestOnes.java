ddc1f5122a560fd751c5d2b643575beb880c8b13
suganth <suganthanchandrika@gmail.com>
2021-02-05 11:06:39 +0800

    Sliding window problem

---
+++ src/slidingWindow/LongestOnes.java
@@ -0,0 +1,16 @@
+package slidingWindow;
+
+/**
+ * 1004. Max Consecutive Ones III
+ */
+public class LongestOnes {
+    public static void main(String[] args) {
+
+    }
+
+    static int longestOnes(int[] A, int K) {
+        int answer = 0;
+
+        return answer;
+    }
+}
