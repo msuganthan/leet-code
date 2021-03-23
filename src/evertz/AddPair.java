2138c2421122162c1651634c75ac7d906e423e36
suganth <suganthanchandrika@gmail.com>
2021-03-23 12:09:06 +0800

    Evertz bangalore questions

---
+++ src/evertz/AddPair.java
@@ -0,0 +1,21 @@
+package evertz;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AddPair {
+    public static void main(String[] args) {
+        //[1, 2, 3, 4, 5, 6] --> [3, 7, 11]
+        new AddPair().add(Arrays.asList(1, 2, 3, 4, 5, 6, 7))
+                .stream()
+                .forEach(System.out::println);
+    }
+
+    public List<Integer> add(List<Integer> nums) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.size(); i = i + 2)
+            result.add(i + 1 < nums.size() ? nums.get(i) + nums.get(i + 1) : nums.get(i));
+        return result;
+    }
+}
--- src/recursion/IncreasingBST.java
+++ src/recursion/IncreasingBST.java
@@ -3,6 +3,13 @@ package recursion;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 897. Increasing Order Search Tree
+ *
+ * Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now
+ * the root of the tree, and every node has no left child and only one right child.
+ *
+ */
 public class IncreasingBST {
 
     public static void main(String[] args) {
@@ -24,6 +31,25 @@ public class IncreasingBST {
         inOrder(treeNode.left, values);
         values.add(treeNode.val);
         inOrder(treeNode.right, values);
+    }
+
+    TreeNode current;
+    public TreeNode increasingBST1(TreeNode root) {
+        TreeNode ans = new TreeNode(0);
+        current = ans;
+        iterateRecursively(root);
+        return current.right;
+    }
+
+    void iterateRecursively(TreeNode node) {
+        if (node == null)
+            return;
+
+        iterateRecursively(node.left);
+        node.left = null;
+        current.right = node;
+        current = node;
+        iterateRecursively(node.right);
     }
 
     public class TreeNode {
