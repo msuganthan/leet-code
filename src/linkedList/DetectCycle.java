b2aa5e9f3774f3e2792770086c8ce9733af02197
smadhavan <suganthanchandrika@gmail.com>
2022-01-13 12:14:57 +0530

    Linked List problems

--- src/linkedList/DetectCycle.java
+++ src/linkedList/DetectCycle.java
@@ -1,47 +1,36 @@
 package linkedList;
 
+/**
+ * 141. Linked List Cycle
+ *
+ * https://leetcode.com/problems/linked-list-cycle/
+ *
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ *
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously
+ * following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer
+ * is connected to. Note that pos is not passed as a parameter.
+ *
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ */
 public class DetectCycle {
 
-    public static void main(String[] args) {
-        ListNode listNode3 = new ListNode(3);
-        ListNode listNode2 = new ListNode(2);
-        ListNode listNode1 = new ListNode(1);
-        ListNode listNode0 = new ListNode(0);
-        listNode0.next = listNode1;
-        listNode1.next = listNode2;
-        listNode2.next = listNode3;
-        listNode3.next = listNode0;
-
-        hasCycle(listNode0);
-    }
-
     static boolean hasCycle(ListNode head) {
+        if(head == null) return false;
         ListNode slow = head;
         ListNode fast = head;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
+        while(slow.next != null && fast.next.next != null) {
             slow = slow.next;
-
-            if (slow == fast)
+            fast = fast.next.next;
+            if(slow == fast) {
                 return true;
+            }
         }
         return false;
     }
 
-    static public class ListNode {
+    private class ListNode {
         int val;
         ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
     }
 }
