4948f5e25c9d9a455db0254fc7134272228c16b8
smadhavan <smadhavan@evertz.com>
2023-08-06 23:42:30 -0400

    More merge sort problems

--- java/priorityqueue/MergeKLists.java
+++ java/priorityqueue/MergeKLists.java
@@ -5,26 +5,26 @@ import java.util.PriorityQueue;
 
 public class MergeKLists {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(a -> a.val));
-        for (ListNode listNode: lists) {
-            if(listNode != null)
-                priorityQueue.add(listNode);
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a.val));
+        for(ListNode node: lists) {
+            if(node != null) {
+                queue.add(node);
+            }
         }
-
         ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
-
-        while (!priorityQueue.isEmpty()){
-            tail.next = priorityQueue.poll();
+        while(!queue.isEmpty()) {
+            tail.next = queue.poll();
             tail = tail.next;
 
-            if (tail.next!=null)
-                priorityQueue.add(tail.next);
+            if(tail.next != null) {
+                queue.add(tail.next);
+            }
         }
         return dummy.next;
     }
 
-    static public class ListNode {
+    static class ListNode {
         int val;
         ListNode next;
 
