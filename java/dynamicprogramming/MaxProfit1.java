2cbd42ddeed0c578db9ca4d8ef544a381692bcf4
smadhavan <smadhavan@evertz.com>
2023-08-08 23:16:25 -0400

    Stack problems

--- java/dynamicprogramming/MaxProfit1.java
+++ java/dynamicprogramming/MaxProfit1.java
@@ -11,20 +11,44 @@ package dynamicprogramming;
  * (i.e., you must sell the stock before you buy again).
  */
 public class MaxProfit1 {
-    public static void main(String[] args) {
-
-    }
-
     public int maxProfit(int[] prices) {
-        int buy1 = Integer.MAX_VALUE, buy2 = Integer.MAX_VALUE;
-        int sell1 = 0, sell2 = 0;
-        for (int i = 0; i < prices.length; i++) {
-            buy1 = Math.min(buy1, prices[i]);
-            sell1 = Math.max(sell1, prices[i] - buy1);
-
-            buy2 = Math.min(buy2, prices[i] - sell1);
-            sell2 = Math.max(sell2, prices[i] - buy2);
+        int result = 0;
+        int lowest = prices[0];
+        for (int price : prices) {
+            if (price < lowest) {
+                lowest = price;
+            }
+            result = Math.max(result, price - lowest);
         }
-        return sell2;
+        return result;
     }
+
+    /**
+     *     def maxProfit(self, prices: List[int]) -> int:
+     *         res = 0
+     *
+     *         lowest = prices[0]
+     *         for price in prices:
+     *             if price < lowest:
+     *                 lowest = price
+     *             res = max(res, price - lowest)
+     *         return res
+     */
+
+    /**
+     * func maxProfit(prices []int) int {
+     *     result := 0
+     *     lowest := prices[0]
+     *
+     *     for _, price := range prices {
+     *         if price < lowest {
+     *             lowest = price
+     *         }
+     *         if price - lowest > result {
+     *             result = price - lowest
+     *         }
+     *     }
+     *     return result
+     * }
+     */
 }
--- java/stack/IsValid.java
+++ java/stack/IsValid.java
@@ -1,6 +1,5 @@
 package stack;
 
-import java.util.HashMap;
 import java.util.Stack;
 
 public class IsValid {
@@ -11,16 +10,63 @@ public class IsValid {
         }
         Stack<Character> stack = new Stack<>();
         for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
-                stack.add(s.charAt(i));
-            } else if(s.charAt(i) == ')' && (stack.isEmpty() || stack.pop() != '(')) {
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '[') {
+                stack.add(c);
+            } else if(c == ')' && (stack.isEmpty() || stack.pop() != '(')) {
                 return false;
-            } else if(s.charAt(i) == ']' && (stack.isEmpty() || stack.pop() != '[')) {
+            } else if(c == ']' && (stack.isEmpty() || stack.pop() != '[')) {
                 return false;
-            } else if(s.charAt(i) == '}' && (stack.isEmpty() || stack.pop() != '{')) {
+            } else if(c == '}' && (stack.isEmpty() || stack.pop() != '{')) {
                 return false;
             }
         }
-        return stack.isEmpty();
+        return stack.empty();
     }
+
+    /**
+     *     def isValid(self, s: str) -> bool:
+     *         if len(s) == 1:
+     *             return False
+     *
+     *         stack = []
+     *         for c in s:
+     *             if c in "({[":
+     *                 stack.append(c)
+     *             elif c == ')' and (not stack or stack.pop() != '('):
+     *                 return False
+     *             elif c == ']' and (not stack or stack.pop() != '['):
+     *                 return False
+     *             elif c == '}' and (not stack or stack.pop() != '{'):
+     *                 return False
+     *         return not stack
+     */
+
+    /**
+     * func isValid(s string) bool {
+     *     if len(s) == 1 {
+     *         return false
+     *     }
+     *
+     *     stack := list.New()
+     *     bracketPairs := map[rune]rune{
+     *         ')':'(',
+     *         '}':'{',
+     *         ']':'[',
+     *     }
+     *
+     *     for _, c := range s {
+     *         switch c {
+     *         case '(', '{', '[':
+     *             stack.PushBack(c)
+     *         case ')', '}', ']':
+     *             if stack.Len() == 0 || stack.Back().Value != bracketPairs[c] {
+     *                 return false
+     *             }
+     *             stack.Remove(stack.Back())
+     *         }
+     *     }
+     *     return stack.Len() == 0
+     * }
+     */
 }
