9d5d8874ce13171cebfedfd70750cfe750fce93e
smadhavan <smadhavan@evertz.com>
2023-08-10 20:24:45 -0400

    Tree problem

--- java/dfs/InvertTree.java
+++ java/dfs/InvertTree.java
@@ -1,13 +1,32 @@
 package dfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class InvertTree {
     public TreeNode invertTree(TreeNode root) {
-        if (root == null)
-            return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root != null){
+            queue.offer(root);
+        }
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                TreeNode current = queue.remove();
 
-        swap(root);
-        invertTree(root.left);
-        invertTree(root.right);
+                TreeNode temp = current.left;
+                current.left = current.right;
+                current.right = temp;
+
+                if(current.left != null) {
+                    queue.add(current.left);
+                }
+
+                if(current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+        }
         return root;
     }
 
