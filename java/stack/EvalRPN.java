48238bfd60f858b7c7b24ad35c1ae2a21e092229
smadhavan <smadhavan@evertz.com>
2023-08-27 18:02:03 -0400

    Stack problems

--- java/stack/EvalRPN.java
+++ java/stack/EvalRPN.java
@@ -29,4 +29,65 @@ public class EvalRPN {
         }
         return stack.pop();
     }
+
+    /**
+     *      def evalRPN(self, tokens: List[str]) -> int:
+     *         stack = []
+     *         for token in tokens:
+     *             if token == "+":
+     *                 stack.append(stack.pop() + stack.pop())
+     *             elif token == "-":
+     *                 b = stack.pop()
+     *                 a = stack.pop()
+     *                 stack.append(a - b)
+     *             elif token == "*":
+     *                 stack.append(stack.pop() * stack.pop())
+     *             elif token == "/":
+     *                 b = stack.pop()
+     *                 a = stack.pop()
+     *                 stack.append(int(float(a) / b))
+     *             else:
+     *                 stack.append(int(token))
+     *
+     *         return stack.pop()
+     */
+
+    /**
+     * func evalRPN(tokens []string) int {
+     *     stack := make([]int, 0)
+     *     var a, b int
+     *     for _, token := range tokens {
+     *         switch token {
+     *             case "+":
+     *                 b = stack[len(stack) - 1]
+     *                 stack = stack[:len(stack) - 1]
+     *                 a = stack[len(stack) - 1]
+     *                 stack = stack[:len(stack) - 1]
+     *                 stack = append(stack, a + b)
+     *             case "-":
+     *                 b = stack[len(stack) - 1]
+     *                 stack = stack[:len(stack) - 1]
+     *                 a = stack[len(stack) - 1]
+     *                 stack = stack[:len(stack) - 1]
+     *                 stack = append(stack, a - b)
+     *             case "*":
+     *                 b = stack[len(stack) - 1]
+     *                 stack = stack[:len(stack) - 1]
+     *                 a = stack[len(stack) - 1]
+     *                 stack = stack[:len(stack) - 1]
+     *                 stack = append(stack, a * b)
+     *             case "/":
+     *                 b = stack[len(stack) - 1]
+     *                 stack = stack[:len(stack) - 1]
+     *                 a = stack[len(stack) - 1]
+     *                 stack = stack[:len(stack) - 1]
+     *                 stack = append(stack, a / b)
+     *             default:
+     *                 num, _ := strconv.Atoi(token)
+     *                 stack = append(stack, num)
+     *         }
+     *     }
+     *     return stack[0];
+     * }
+     */
 }
