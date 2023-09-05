6f061739024284419fea316cbf6c28aae9339fe3
smadhavan <smadhavan@evertz.com>
2023-09-05 08:10:19 -0400

    Graph problems

--- java/graph/NumberOfIslands.java
+++ java/graph/NumberOfIslands.java
@@ -1,32 +1,20 @@
-package tree;
+package graph;
 
 public class NumberOfIslands {
 
-    public static void main(String[] args) {
-        numIslands(new char[][]{
-                {'1', '1', '0', '0', '0'},
-                {'1', '1', '0', '0', '0'},
-                {'0', '0', '1', '0', '0'},
-                {'0', '0', '0', '1', '1'}
-        });
-    }
-
-    static int numIslands(char[][] grid) {
-
+    int numIslands(char[][] grid) {
         int count = 0;
-
         for (int i = 0; i < grid.length; i++)
             for (int j = 0; j < grid[i].length; j++)
                 if (grid[i][j] == '1') {
                     count += 1;
                     callBFS(grid, i, j);
                 }
-
         return count;
 
     }
 
-    static void callBFS(char[][] grid, int i, int j) {
+    void callBFS(char[][] grid, int i, int j) {
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0')
             return;
         grid[i][j] = '0';
